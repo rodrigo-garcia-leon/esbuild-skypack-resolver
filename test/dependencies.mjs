2f03bf67a9531d3d5f323c99b5fa43c2d819cbb0
Rodrigo García <yo@rodrigogarcia.me>
2021-06-04 00:03:25 +0200

    Inline mock results

--- test/dependencies.mjs
+++ test/dependencies.mjs
@@ -5,19 +5,17 @@ import { getDependencies } from "../src/dependencies.js";
 
 const __dirname = dirname(new URL(import.meta.url).pathname);
 
-const DEPENDENCIES = {
-  "@lit/reactive-element": "1.0.0-rc.2",
-  "@types/trusted-types": "1.0.6",
-  lit: "2.0.0-rc.2",
-  "lit-element": "3.0.0-rc.2",
-  "lit-html": "2.0.0-rc.3",
-};
-
 describe("getDependencies", function () {
   it("ok", async function () {
     const file = join(__dirname, "./example/package-lock.json");
     const dependencies = await getDependencies(file);
 
-    expect(dependencies).to.deep.equal(DEPENDENCIES);
+    expect(dependencies).to.deep.equal({
+      "@lit/reactive-element": "1.0.0-rc.2",
+      "@types/trusted-types": "1.0.6",
+      lit: "2.0.0-rc.2",
+      "lit-element": "3.0.0-rc.2",
+      "lit-html": "2.0.0-rc.3",
+    });
   });
 });
